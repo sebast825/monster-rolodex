f64adc63d584a28c340af2b4d352423eb2dd67f3
sebast825 <sebamolina825@gmail.com>
2022-06-17 13:53:20 -0300

    card list

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,21 +1,23 @@
 import { Component } from "react";
-
+import "./card-list.styles.css"
 class CardList extends Component{
 	render(){		
 		const {monsters} = this.props;
 		console.log(monsters)
 		return(
-			<div>
-			{monsters.map((monster)=>(				
-				<div class="card-container" key={monster.id}>
+			<div class="card-list">
+			{monsters.map((monster)=>{
+				const {name,id,email} = monster;
+				return(				
+				<div class="card-container" key={id}>
 					<img
-						alt={`monster ${monster.name}`}
-						src={`https://robohash.org/${monster.id}?set=set2`}
+						alt={`monster ${name}`}
+						src={`https://robohash.org/${id}?set=set2`}
 					/>
-					<h2>{monster.name}</h2>
-					<p>{monster.email} </p>
+					<h2>{name}</h2>
+					<p>{email} </p>
 				</div>	
-			))
+			)})
 			}
 			</div>
 		);		
